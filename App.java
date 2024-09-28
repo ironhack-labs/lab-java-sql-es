c36f1b66f5eae724e01fb265dcb3a24a669be5be
Eric1999 <ericdiaz63942@gmail.com>
2024-09-28 15:36:37 +0200

    finish lab

--- App.java
+++ App.java
@@ -16,7 +16,9 @@ INSERT INTO Articulo (titulo, recuento_de_palabras, vistas, id_autor) VALUES
         ('Trucos de arte caseros', 1002, 193, 1),
         ('Renovación de pisos de madera', 1571, 7542, 3);
 
--- Crear la tabla Clientes
+------------------------------------------
+
+-- Crear tabla de clientes
 CREATE TABLE Clientes (
         id_cliente INT AUTO_INCREMENT PRIMARY KEY,
         nombre VARCHAR(255) NOT NULL,
@@ -24,69 +26,106 @@ CREATE TABLE Clientes (
         kilometraje_total INT NOT NULL
 );
 
--- Crear la tabla Aeronaves
+
+-- Crear tabla de vuelos
+CREATE TABLE Vuelos (
+        id_vuelo INT AUTO_INCREMENT PRIMARY KEY,
+        numero_vuelo VARCHAR(50) NOT NULL,
+        kilometraje_vuelo INT NOT NULL
+);
+
+--Crear tabla de aeronaves
 CREATE TABLE Aeronaves (
         id_aeronave INT AUTO_INCREMENT PRIMARY KEY,
         modelo VARCHAR(255) NOT NULL,
         total_asientos INT NOT NULL
 );
 
--- Crear la tabla Vuelos
-CREATE TABLE Vuelos (
-        id_vuelo INT AUTO_INCREMENT PRIMARY KEY,
-        numero_vuelo VARCHAR(50) NOT NULL,
-        id_aeronave INT,
-        kilometraje_vuelo INT NOT NULL,
-         FOREIGN KEY (id_aeronave) REFERENCES Aeronaves(id_aeronave)
-        );
-
--- Crear la tabla ClienteVuelo (relación muchos a muchos)
-CREATE TABLE ClienteVuelo (
+-- Crear tabla de Reservas
+CREATE TABLE Reservas (
+        id_reserva INT AUTO_INCREMENT PRIMARY KEY,
         id_cliente INT,
         id_vuelo INT,
+        id_aeronave INT,
+        cantidad_pedida INT NOT NULL,
         FOREIGN KEY (id_cliente) REFERENCES Clientes(id_cliente),
-      FOREIGN KEY (id_vuelo) REFERENCES Vuelos(id_vuelo),
-        PRIMARY KEY (id_cliente, id_vuelo)
-);
+        FOREIGN KEY (id_vuelo) REFERENCES Vuelos(id_vuelo),
+        FOREIGN KEY (id_aeronave) REFERENCES Aeronaves(id_aeronave)
+        );
 
--- Insertar datos en la tabla Clientes
+-- Insertar datos tabla clientes
 INSERT INTO Clientes (nombre, estado, kilometraje_total) VALUES
-        ('Riviera Agustina', 'Plata', 115235),
-        ('Alaina Sepulvida', 'Ninguno', 6008),
-        ('Tom Jones', 'Oro', 205767),
-        ('Sam Río', 'Ninguno', 2653),
-        ('Jessica James', 'Plata', 127656),
-        ('Ana Janco', 'Plata', 136773),
-        ('Jennifer Cortez', 'Oro', 300582),
-        ('Janco Cristiano', 'Plata', 14642);
+        ('Agustine Riviera', 'Silver', 115235),
+        ('Alaina Sepulvida', 'None', 6008),
+        ('Tom Jones', 'Gold', 205767),
+        ('Sam Rio', 'None', 2653),
+        ('Jessica James', 'Silver', 127656),
+        ('Ana Janco', 'Silver', 136773),
+        ('Jennifer Cortez', 'Gold', 300582),
+        ('Christian Janco', 'Silver', 14642);
 
--- Insertar datos en la tabla Aeronaves
+-- Insertar datos tabla vuelos
+INSERT INTO Vuelos (numero_vuelo, kilometraje_vuelo) VALUES
+        ('DL143', 135),
+        ('DL122', 4370),
+        ('DL53', 2078),
+        ('DL222', 1765),
+        ('DL37', 531);
+
+-- Insertar datos tabla aeronaves
 INSERT INTO Aeronaves (modelo, total_asientos) VALUES
         ('Boeing 747', 400),
         ('Airbus A330', 236),
         ('Boeing 777', 264);
 
--- Insertar datos en la tabla Vuelos
-INSERT INTO Vuelos (numero_vuelo, id_aeronave, kilometraje_vuelo) VALUES
-        ('DL143', 1, 135),
-        ('DL122', 2, 4370),
-        ('DL53', 3, 2078),
-        ('DL222', 3, 1765),
-        ('DL37', 1, 531);
+-- Insertar datos tabla reserva
+INSERT INTO Reservas (id_cliente, id_vuelo, id_aeronave, cantidad) VALUES
+((SELECT id_cliente FROM Clientes WHERE nombre =
+        'Agustine Riviera'), (SELECT id_vuelo FROM Vuelos WHERE numero_vuelo = 'DL143'), (SELECT id_aeronave FROM Aeronaves WHERE modelo = 'Boeing 747'), 1),
+        ((SELECT id_cliente FROM Clientes WHERE nombre =
+        'Agustine Riviera'), (SELECT id_vuelo FROM Vuelos WHERE numero_vuelo = 'DL122'), (SELECT id_aeronave FROM Aeronaves WHERE modelo = 'Airbus A330'), 1),
+        ((SELECT id_cliente FROM Clientes WHERE nombre =
+        'Alaina Sepulvida'), (SELECT id_vuelo FROM Vuelos WHERE numero_vuelo = 'DL122'), (SELECT id_aeronave FROM Aeronaves WHERE modelo = 'Airbus A330'), 1),
+        ((SELECT id_cliente FROM Clientes WHERE nombre =
+        'Tom Jones'), (SELECT id_vuelo FROM Vuelos WHERE numero_vuelo = 'DL122'), (SELECT id_aeronave FROM Aeronaves WHERE modelo
 
--- Insertar datos en la tabla ClienteVuelo (relación muchos a muchos)
-INSERT INTO ClienteVuelo (id_cliente, id_vuelo) VALUES
-        (1, 1),
-        (1, 2),
-        (2, 2),
-        (3, 2),
-        (3, 3),
-        (4, 1),
-        (3, 4),
-        (5, 1),
-        (5, 2),
-        (6, 4),
-        (7, 4),
-        (4, 5),
-        (8, 4);
+-- obtener Nubero total de vuelos
+SELECT COUNT(*) AS total_vuelos FROM Vuelos;
 
+--Obtener distancia promedio del vuelo
+SELECT AVG(kilometraje_vuelo) AS distancia_promedio FROM Vuelos;
+
+--Obtener el numero promedio de millas voladas por los clienes agrupados por estatus
+SELECT AVG(total_asientos) AS promedio_asientos FROM Aeronaves;
+
+--Obtener el numero maximo de millas voladas por los clientes agrupados por estatus
+        SELECT estado, MAX(kilometraje_total) AS max_millas
+           FROM Clientes
+           GROUP BY estado;
+
+-- Obtener el numero total de aeronaves con el nombre boeing
+SELECT COUNT(*) AS total_boeing
+FROM Aeronaves
+WHERE modelo LIKE '%Boeing%';
+
+--Encontrar todos los vuelos con una distancia de entre 300 y 2000 millas
+SELECT *
+FROM Vuelos
+WHERE kilometraje_vuelo BETWEEN 300 AND 2000;
+
+-- Encontrar la distancia promedio de vuelo
+SELECT c.estado, AVG(v.kilometraje_vuelo) AS distancia_promedio
+FROM Reservas r
+JOIN Clientes c ON r.id_cliente = c.id_cliente
+JOIN Vuelos v ON r.id_vuelo = v.id_vuelo
+GROUP BY c.estado;
+
+--Encontrar la aeronave mas reservada por los miembros del estatus oro
+SELECT a.modelo, COUNT(*) AS num_reservas
+FROM Reservas r
+JOIN Clientes c ON r.id_cliente = c.id_cliente
+JOIN Aeronaves a ON r.id_aeronave = a.id_aeronave
+WHERE c.estado = 'Gold'
+GROUP BY a.modelo
+ORDER BY num_reservas DESC
+LIMIT 1;
